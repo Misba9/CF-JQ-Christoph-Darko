f394e97cc1f869de103dbe5f28495fa0ac90655d
Darko Kolak <kolakdarko@yahoo.com>
2019-08-06 15:27:23 +0200

    advanced

---
+++ advanced/jscript.js
@@ -0,0 +1,24 @@
+$("a").attr("href", "https://www.codefactory.wien")
+
+$("code").css("color", "red")
+
+$("ol > li").css("background-color","#2a7b90")
+
+$("input").val("Express your opinion")
+
+$("img").attr("src", "https://codefactory.wien/wp-content/themes/hestia/cf-images/small-jpg/2-fullstack.jpg")
+
+$("blockquote").css({
+	"color": "orange",
+	"font-style": "italic"})
+
+$("#logo").css("color","black")
+
+$(".gray:first").css("color", "gray")
+
+$(".date").remove()
+
+$("#sidebar > ul ").append("<li><a>New Template</a></li><li><a>Order Template</a></li><li><a>Contact Us</a></li>")
+
+$("p:contains('Be not afraid of life. Believe that life is worth living, and your belief will help create the fact.')").text("When someone says: 'I want a programming language in which I need only to say what I wish, and done', give him a lollipop.")
+
